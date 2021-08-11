a8536c0839f516c580b12d7b31d3807c8496ab1a
Jadeed Kamal <83796140+codetoastje@users.noreply.github.com>
2021-08-12 01:27:20 +0530

    Create Data_Types.py

---
+++ basic/Data_Types.py
@@ -0,0 +1,7 @@
+# there are mainly three types of data types
+# string, int, float, boolean 
+
+name = 'John smith' #is a string data type and both " as well as ' to assign a word to the string variable 
+age = 20 #is integer data type 
+is_new = True #is a boolean data type and can contain True or False value 
+balance = 299.99 #is a float data type and can contain decimal values 
