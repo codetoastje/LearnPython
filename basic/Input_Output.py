724f2ead991c75e9df04c9b5bda48d26b40bcc6e
Jadeed Kamal <83796140+codetoastje@users.noreply.github.com>
2021-08-12 01:29:54 +0530

    Create Input_Output.py

---
+++ basic/Input_Output.py
@@ -0,0 +1,5 @@
+#Values can be inputted into variables using input() function 
+
+name = input("What is your name") #the question to be asked can be put whithin the input function 
+
+print(name) #is used to print the value of the variable 'name' 
